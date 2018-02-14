d2acff0607a0c7aff1ae889baf82e4eb8e6b0603
Anna.Arshinova <z0031jg@8c8590494bbf.hq.target.com>
2018-02-14 08:52:19 -0800

    Added who is the winner control
    - added control to determine if there is a winner
    - added blocking the board if there is a winner
    - added display the winner who won the game

--- app/board/board.js
+++ app/board/board.js
@@ -10,13 +10,14 @@ angular.module('myApp.board', [])
 
           $scope.nextPlayer = 'Player2';
 
+          $scope.win = false;
+
           $scope.board = [
                 [ { value: '-' }, { value: '-' }, { value: '-' } ],
                 [ { value: '-' }, { value: '-' }, { value: '-' } ],
                 [ { value: '-' }, { value: '-' }, { value: '-' } ]
             ];
             self.buttonClick = function buttonClick(cell) {
-                //window.alert("clicked!");
                 if (cell.value=='-'){
                    cell.value = ($scope.player=='Player1') ?  'x': 'o';
                 }
@@ -24,14 +25,21 @@ angular.module('myApp.board', [])
                 else $scope.player='Player1';
 
                 $scope.nextPlayer = ($scope.player==='Player1') ? 'Player2' : 'Player1';
+
+                // watch the changes in the board
+                if ($scope.win) {
+                    $scope.nextPlayer = '';
+                    $scope.player==$scope.player + ' won!'
+                }
             }
 
             self.checkDisabled = function checkDisabled(cell) {
-                return (cell.value!=='-');
+                return (cell.value!=='-') || $scope.win;
             }
 
             self.reset = function reset() {
                 $scope.player='Player1';
+                $scope.win = false;
                 $scope.board = [
                     [ { value: '-' }, { value: '-' }, { value: '-' } ],
                     [ { value: '-' }, { value: '-' }, { value: '-' } ],
@@ -40,8 +48,49 @@ angular.module('myApp.board', [])
             }
 
             $scope.$watch('board', function (newValue, oldValue, scope) {
-                // watch the changes in the board
-               // if (board[0][0]===board[0][1] && board[0][0]==board[0][2])
+                var board = $scope.board;
+                // check rows
+                var win = true;
+                for (var i=0; i<board.length; i++){
+                    win = true;
+                    for (var j=0; j<board.length-1; j++){
+                        if (board[i][j].value !== board[i][j+1].value || board[i][j].value==='-'){
+                            win = false;
+                        }
+                    }
+                    if (win){
+                        $scope.win = true;
+                        break;
+                    }
+                }
+
+                //check columns
+                for (var i=0; i<board.length; i++){
+                    win = true;
+                    for (var j=0; j<board.length-1; j++){
+                        if (board[j][i].value !== board[j+1][i].value || board[i][j].value==='-'){
+                            win = false;
+                        }
+                    }
+                    if (win){
+                        $scope.win = true;
+                        break;
+                    }
+                }
+
+                //check diagonals (0,0) (1,1) (2,2)
+                if (board[0][0].value === board[1][1].value && board[1][1].value === board[2][2].value && board[0][0].value!=='-'){
+                    $scope.win = true;
+                    return;
+                }
+
+                //check diagonals (0,2) (1,1) (2,0)
+                if (board[0][2].value === board[1][1].value && board[1][1].value === board[2][0].value && board[0][2].value!=='-'){
+                    $scope.win = true;
+                    return;
+                }
+
             }, true);
         }
     });
+
